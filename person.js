5dcd5962adbb770657bd7bb0b868a0b93b66b6fd
thatOkeBrett <moyobrett@gmail.com>
2020-04-16 16:08:24 +0200

    project complete

---
+++ person.js
@@ -0,0 +1,13 @@
+class Person{
+  constructor(name,age,gender,interests){
+    this.name = name;
+   this.age = age;
+   this.gender = gender;
+   this.interests = interests;
+  }
+  hello(){
+    console.log(`Hello, my name is ${this.name}  and I am ${this.age} years old.I am ${this.gender} and my interests are ${this.interests}`)
+  }
+}
+let person = new Person('Ryan',30,'male',['being a hardarse','agile', ' and ssd hard drives'] )
+person.hello()
